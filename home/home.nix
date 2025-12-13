5f55cf22bc8f67b3320612f2837ca18616a480f0
Thomas <thomas@anvir.fr>
2025-12-13 23:04:10 +0100

    Clean configuration

--- home/home.nix
+++ home/home.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-#    ./hyprland.nix
+    ./neovim.nix
     ./zellij.nix
   ];
 
@@ -12,29 +12,7 @@
 
   programs.home-manager.enable = true;
 
-  # Install neovim
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-  };
-
-  home.activation.cloneNeovimConfig = config.lib.dag.entryAfter ["writeBoundary"] ''
-    NVIM_DIR="$HOME/.config/nvim"
-    
-    if [ ! -d "$NVIM_DIR" ]; then
-      echo "Cloning neovim config..."
-      ${pkgs.git}/bin/git clone https://github.com/DamyrFr/neovim-config "$NVIM_DIR"
-    else
-      echo "Neovim config already exists, pulling latest..."
-      cd "$NVIM_DIR" && ${pkgs.git}/bin/git pull
-    fi
-  '';
-
   home.packages = with pkgs; [
-    ripgrep
-    fd
     fzf
     nerd-fonts.jetbrains-mono
     kubectl
--- home/zellij.nix
+++ home/zellij.nix
@@ -5,9 +5,9 @@
     enable = true;
   };
 
-  # Fetch zellij config directly from GitHub
+  # Fetch zellij config from GitHub
   home.file.".config/zellij/config.kdl".source = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/DamyrFr/ansible-personal-computer/refs/heads/master/roles/dotfiles/files/config.kdl";
-    sha256 = "1s9bh2n56wsarxbwb8m7hh93fm5krzy670z4wasz70m3k250gmpk";
+    sha256 = "04p1gs77m73244a9iiqbk7ris1ar3fqvny8kq1ani0xd53apv0nv";
   };
 }
