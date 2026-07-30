eccd5c3f47905ab7a683a1d56d2cd93d78d6a37c
DamyR <thomas@anvir.fr>
2026-07-30 15:15:11 +0200

    Add OpenCode bloclist

--- modules/home/agent.nix
+++ modules/home/agent.nix
@@ -15,10 +15,109 @@
     enable = true;
     package = pkgs.opencode;
     extraPackages = with pkgs; [ libnotify alsa-utils ];
-    settings.plugin = [
-      "opencode-mem"
-      "@mohak34/opencode-notifier@latest"
-    ];
+    settings = {
+      plugin = [
+        "opencode-mem"
+        "@mohak34/opencode-notifier@latest"
+      ];
+      permission.bash = {
+        "*" = "ask";
+
+        "kubectl create*" = "deny";
+        "kubectl apply*" = "deny";
+        "kubectl delete*" = "deny";
+        "kubectl patch*" = "deny";
+        "kubectl edit*" = "deny";
+        "kubectl replace*" = "deny";
+        "kubectl scale*" = "deny";
+        "kubectl set*" = "deny";
+        "kubectl label*" = "deny";
+        "kubectl annotate*" = "deny";
+        "kubectl taint*" = "deny";
+        "kubectl cordon*" = "deny";
+        "kubectl uncordon*" = "deny";
+        "kubectl drain*" = "deny";
+        "kubectl rollout restart*" = "deny";
+        "kubectl rollout undo*" = "deny";
+        "kubectl expose*" = "deny";
+        "kubectl autoscale*" = "deny";
+        "kubectl exec*" = "deny";
+        "kubectl cp*" = "deny";
+
+        "helm install*" = "deny";
+        "helm upgrade*" = "deny";
+        "helm uninstall*" = "deny";
+        "helm rollback*" = "deny";
+        "helm delete*" = "deny";
+
+        "flux delete*" = "deny";
+        "flux suspend*" = "deny";
+        "flux resume*" = "deny";
+        "flux reconcile*" = "deny";
+
+        "gcloud *create*" = "deny";
+        "gcloud *delete*" = "deny";
+        "gcloud *update*" = "deny";
+        "gcloud *set*" = "deny";
+        "gcloud *patch*" = "deny";
+        "gcloud *add*" = "deny";
+        "gcloud *remove*" = "deny";
+        "gcloud *deploy*" = "deny";
+        "gcloud *import*" = "deny";
+        "gcloud *undelete*" = "deny";
+        "gcloud *reset*" = "deny";
+        "gcloud *start*" = "deny";
+        "gcloud *stop*" = "deny";
+        "gcloud *restart*" = "deny";
+        "gcloud *promote*" = "deny";
+        "gcloud *rollback*" = "deny";
+
+        "aws *create*" = "deny";
+        "aws *delete*" = "deny";
+        "aws *put*" = "deny";
+        "aws *update*" = "deny";
+        "aws *modify*" = "deny";
+        "aws *terminate*" = "deny";
+        "aws *remove*" = "deny";
+        "aws *attach*" = "deny";
+        "aws *detach*" = "deny";
+        "aws *register*" = "deny";
+        "aws *deregister*" = "deny";
+        "aws *revoke*" = "deny";
+        "aws *authorize*" = "deny";
+        "aws *reboot*" = "deny";
+        "aws *run-instances*" = "deny";
+        "aws *stop-instances*" = "deny";
+        "aws *start-instances*" = "deny";
+        "aws s3 rm*" = "deny";
+        "aws s3 mv*" = "deny";
+        "aws s3 cp*" = "deny";
+        "aws s3 sync*" = "deny";
+        "aws s3 rb*" = "deny";
+        "aws s3api put-object*" = "deny";
+        "aws s3api delete-object*" = "deny";
+
+        "terraform apply*" = "deny";
+        "terraform destroy*" = "deny";
+        "terraform import*" = "deny";
+        "terraform state rm*" = "deny";
+        "terraform state mv*" = "deny";
+        "terraform state push*" = "deny";
+        "terraform taint*" = "deny";
+        "terraform untaint*" = "deny";
+        "terraform force-unlock*" = "deny";
+
+        "tofu apply*" = "deny";
+        "tofu destroy*" = "deny";
+        "tofu import*" = "deny";
+        "tofu state rm*" = "deny";
+        "tofu state mv*" = "deny";
+        "tofu state push*" = "deny";
+        "tofu taint*" = "deny";
+        "tofu untaint*" = "deny";
+        "tofu force-unlock*" = "deny";
+      };
+    };
   };
 
   home.activation.rtkInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
